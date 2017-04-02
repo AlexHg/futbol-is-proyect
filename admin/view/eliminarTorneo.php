529365482eae0ade689ea69a2ead09d3e6397549
Luis Fernando Santuario Parra <santuarioparral@hotmail.com>
2017-04-02 17:16:32 -0500

    arreglada vista para eliminar toreno

--- admin/view/eliminarTorneo.php
+++ admin/view/eliminarTorneo.php
@@ -1,3 +1,27 @@
 <div id="content-title">
     <h2>Eliminar Torneo</h2>
 </div>
+
+<div id="content-body">
+    <div class="row center-xs center-md center-lg">
+        <div class="box">
+            <form method="post" class="form-control-cont" role="form" action="eliminarTorneo">
+                <div class="form-control-cont">
+
+                    <label class="control-label">Nombre del Torneo:</label>
+
+                    <select class="form-control full">
+                                    <option>Liga MX</option>
+                                    <option>Premier League</option>
+                                    <option>Bundesliga</option>
+                                    <option>Escompis</option>
+                    </select>
+                </div>
+
+                <center>
+                    <button type="button" onclick="ConfirmDemo()" class="btn btn-danger btn-rounded">Eliminar</button>
+                </center>
+            </form>
+        </div>
+    </div>
+</div>
