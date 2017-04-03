a106dfbd643c5edd350f57e3e22be706cc7dd206
Luis Fernando Santuario Parra <santuarioparral@hotmail.com>
2017-04-02 19:15:24 -0500

    arreglada vista de eliminar torneo

--- admin/view/eliminarTorneo.php
+++ admin/view/eliminarTorneo.php
@@ -5,21 +5,25 @@
 <div id="content-body">
     <div class="row center-xs center-md center-lg">
         <div class="box">
+           <?php
+            adiosTorneo();
+            ?>
             <form method="post" class="form-control-cont" role="form" action="eliminarTorneo">
                 <div class="form-control-cont">
 
                     <label class="control-label">Nombre del Torneo:</label>
 
-                    <select class="form-control full">
-                                    <option>Liga MX</option>
-                                    <option>Premier League</option>
-                                    <option>Bundesliga</option>
-                                    <option>Escompis</option>
-                    </select>
+                    <div class="form-control-cont">
+                        <select onChange="change(this.value)" name="ni" class="form-control full" id="TorneoSelect">
+                            <?php
+                                listaTorneos();
+                            ?>
+                        </select>
+                    </div>
                 </div>
 
                 <center>
-                    <button type="button" onclick="ConfirmDemo()" class="btn btn-danger btn-rounded">Eliminar</button>
+                    <input type="submit" class="btn btn-success" name="Aceptar" value="Aceptar"/>
                 </center>
             </form>
         </div>
