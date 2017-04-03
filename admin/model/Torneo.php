cb2386aeb7ad544ceec9cafc60a1a780eee3bb43
Luis Fernando Santuario Parra <santuarioparral@hotmail.com>
2017-04-02 19:15:06 -0500

    agregado funcion a modelo de torneo

--- admin/model/Torneo.php
+++ admin/model/Torneo.php
@@ -12,4 +12,28 @@ class Torneo{
         }
         mysqli_close($conexion);
     }
+
+public static function getTorneos(){
+        $conexion = Database::connect();
+        $consulta ="SELECT nombre from Torneo;";
+        if ($resultado=$conexion->query($consulta)) {
+            return $resultado;
+        } else {
+            return "Error: " . mysqli_error($conexion);
+        }
+        mysqli_close($conexion);
+    }
+
+     public static function eliminarTorneo($nombreTorneo){
+        $conexion = Database::connect();
+        if ($resultado=$conexion->query("Call p2('$nombreTorneo')")) {
+            return 1;
+        } else {
+            return "Error: " . mysqli_error($conexion);
+        }
+        mysqli_close($conexion);
+    }
+
+
 }
+
