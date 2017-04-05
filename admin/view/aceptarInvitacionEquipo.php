5380c29f46c57f278dccd37558967f6a583ed6db
AMPS95 <amarianaps95@gmail.com>
2017-04-04 23:16:02 -0500

    Update aceptarInvitacionEquipo.php

--- admin/view/aceptarInvitacionEquipo.php
+++ admin/view/aceptarInvitacionEquipo.php
@@ -2,92 +2,113 @@
     <h2>Aceptar Invitacion de Equipo</h2>
 </div>
 <div id="content-body">
-    <div class="row center-xs center-sm center-md center-lg">
-        <div class="col-xs-12  col-md-5  col-lg-5">
-            <div class="box">
-                <form class="form-horizontal" role="form_datos_capitan">
-                    <div class="form">
-                        <label class="control-label">Unirse al equipo</label>
-                        <div class="form-control-cont ">
-                            <input type="text" style="text-align:center" class="form-control full" readonly="" value="Cerverus">
-                        </div>
-                    </div>
-                    <div class="row ">
-                        <div class="col-xs-1 col-md-1 col-lg-1">
-                            <a href="#" id="profileCapitan">
-                                <img src="img/capitan.jpg" alt=""></a>
-                        </div>
-                        <div class="col-xs-11 col-md-11 col-lg-11">
-                            <div class="form" style="text-align:center">
-                                <label class="control-label">Capitan del equipo</label>
-                                <div class="form-control-cont ">
-                                    <input type="text" class="form-control full" readonly="" value="Alina Perez">
-                                </div>
-                            </div>
+      <table class="table table-striped " style="overflow-x: hidden; overflow-y: scroll;">
+                        <thead>
+                            <tr>
+                                <th>Elegir</th>
+                                <th>Equipo</th>
+                                <th>Capitán</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>
+                                    <div class="checkbox checkbox-primary">
+                                        <input id="checkbox1" type="checkbox">
+                                        <label for="checkbox1">
+                                            
+                                        </label>
+                                    </div>
+                                </th>
+                                <td>Equipo</td>
+                                 <td>IMAGE</td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <div class="checkbox checkbox-primary">
+                                        <input id="checkbox2" type="checkbox">
+                                        <label for="checkbox2">
+                                            
+                                        </label>
+                                    </div>
+                                </th>
+                                
+                                <td>Pablo Bautista</td>
+                                <td>IMAGE</td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <div class="checkbox checkbox-primary">
+                                        <input id="checkbox3" type="checkbox">
+                                        <label for="checkbox3">
 
+                                        </label>
+                                    </div>
+                                </th>
+                                
+                                <td>Alex Cruz</td>
+                                <td>IMAGE</td>
+                            </tr>
+                        </tbody>
+                    </table> 
+                     <div class="row center-xs center-sm center-md center-lg">
+                        <div class="ol-xs-3 col-md-3 col-lg-3">
+                            <div class="box">
+                                <button id="cancelarInvitacion" type="button" class="btn btn-success" data-type="alerta">Aceptar</button>
+                            </div>
+                        </div>
+                        <div class="col-xs-3 col-md-3 col-lg-3">
+                            <div class="box">
+                                <button id="aceptarInvitacion" type="button" class="btn btn-danger" data-type="alerta2">Rechazar</button>
+                            </div>
                         </div>
                     </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <div class="row center-xs center-sm center-md center-lg">
-        <div class="ol-xs-3 col-md-3 col-lg-3">
-            <div class="box">
-                <button id="cancelarInvitacion" type="button" class="btn btn-danger">Cancelar</button>
-            </div>
-        </div>
-        <div class="col-xs-3 col-md-3 col-lg-3">
-            <div class="box">
-                <button id="aceptarInvitacion" type="button" class="btn btn-success">Aceptar</button>
-            </div>
-        </div>
     </div>
     <br/>
     <hr>
     <br/>
-    <div class="row center-xs center-sm center-md center-lg">
-        <div class="col-xs-12  col-md-5  col-lg-5">
-            <div class="box">
-                <form class="form-horizontal" role="form_datos_capitan">
-                    <div class="form">
-                        <label class="control-label">Unirse al equipo</label>
-                        <div class="form-control-cont ">
-                            <input type="text" style="text-align:center" class="form-control full" value="ESCOM United" readonly="">
+<!--Mensaje acptar solicitud para 1-->
+ <div class="overlay-container">
+                        <div class="window-container alerta">
+                             <h3>Confirmación de solicitud de Equipo</h3> 
+                           	¿Estas seguro de que deseas pertenecer a este equipo?
+                            <br/>
+                            <center>
+                            <button class="btn btn-success close" class="window-container aceptar" >Aceptar</button>
+                            <button class="btn btn-danger" >Cancelar</button>
+                            </center>
                         </div>
-                    </div>
-                    <div class="row ">
-                        <div class="col-xs-1 col-md-1 col-lg-1">
-                            <a href="#" id="profileCapitan">
-                                <img src="img/capitan2.jpg" alt="">
-                            </a>
+</div>
+<div class="overlay-container">
+                        <div class="window-container aceptar">
+                             <h3>Solicitud Aceptada</h3> 
+                            Ahora eres miembro de este equipo <br/>
+                            <br/>
+                            <center>
+                            <button class="btn btn-success close" >Aceptar</button>
+                            </center>
                         </div>
-                        <div class="col-xs-11 col-md-11 col-lg-11">
-                            <div class="form" style="text-align:center">
-                                <label class="control-label">Capitan del equipo</label>
-                                <div class="form-control-cont ">
-                                    <input type="text" class="form-control full" readonly="" value="Rolando Romero">
-                                </div>
-                            </div>
+</div>
+<!--Mensaje acptar solicitud para 2-->
+ <div class="overlay-container">
+                        <div class="window-container alerta">
+                             <h3>Confirmación de Rechazo de Equipo</h3> 
+                           	¿Estas seguro de que deseas rechazar a este equipo?
+                            <br/>
+                            <center>
+                            <button class="btn btn-success close" class="window-container rechazar" >Aceptar</button>
+                            <button class="btn btn-danger" >Cancelar</button>
+                            </center>
+                        </div>
+</div>
+<div class="overlay-container">
+                        <div class="window-container rechazar">
+                             <h3>Solicitud Rechazada</h3> 
+                            La solicitud ha sido rechazada <br/>
+                            <br/>
+                            <center>
+                            <button class="btn btn-success close" >Aceptar</button>
+                            </center>
+                        </div>
+</div>
 
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <div class="row center-xs center-sm center-md center-lg">
-        <div class="ol-xs-3 col-md-3 col-lg-3">
-            <div class="box">
-                <button id="cancelarInvitacion" type="button" class="btn btn-danger">Cancelar</button>
-            </div>
-        </div>
-        <div class="col-xs-3 col-md-3 col-lg-3">
-            <div class="box">
-                <button id="aceptarInvitacion" type="button" class="btn btn-success">Aceptar</button>
-            </div>
-        </div>
-    </div>
-    <br/>
-    <hr>
-    <br/>
