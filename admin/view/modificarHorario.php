8f6bfcba21ccbae6ab60773a00230e553dc09824
AMPS95 <amarianaps95@gmail.com>
2017-04-09 16:01:20 -0500

    Update modificarHorario.php

--- admin/view/modificarHorario.php
+++ admin/view/modificarHorario.php
@@ -1,16 +1,17 @@
 <div id="content-title">
-    <h2>Modificar Horario</h2>
+    <h2>Modificar Horario de Partido</h2>
 </div>
 <div id="content-body">
 
 
     <div align="center">
         <div>
-            <h3>Seleccione su nuevo horario</h3>
+            <h3>Selecciona el nuevo horario</h3>
             <br>
             <br>
         </div>
-        <form class="form-control" role="form">
+       <div id="content-body">
+         <form class="form-control" role="form" style="width: 90%">
             <div class="form-group">
                 <label class="control-label">Hora</label>
                 <div class="form-control-cont">
@@ -33,6 +34,16 @@
                     </select>
                 </div>
             </div>
+            <div class="form-group">
+            <label class="control-label">TORNEO</label>
+            <div class="form-control-cont">
+                <select class="form-control full">
+                    <option>Torneo1</option>
+                    <option>Torneo2</option>
+                    <option>Torneo3</option>
+                </select>
+            </div>
+        </div>
         </form>
         <br>
         <br>
