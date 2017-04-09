c90f8a76c8228b75439483268e011edf87612330
AMPS95 <amarianaps95@gmail.com>
2017-04-09 14:55:31 -0500

    Update aceptarPeticiones.php

--- admin/view/aceptarPeticiones.php
+++ admin/view/aceptarPeticiones.php
@@ -1,36 +1,37 @@
-<div id="content-title">
-    <h2>Peticiones de inscripción a Torneo</h2>
+  <div id="content-title">
+    <h2>Aceptar Petición</h2>
 </div>
-
 <div id="content-body">
-    <div>
-        <h3>Solicitudes recientes</h3>
-    </div>
-    <p>Seleccione las solicitudes para aceptarlas o rechazarlas</p>
-    <br>
-    <div style=" border-top-style: solid;border-bottom-style: solid;">
-    <br>
-        <form class="form-horizontal" role="form">
-            <div class="checkbox checkbox-info">
-                <input id="checkbox4" type="checkbox">
-                    <label for="checkbox4">Alina Perez desea inscribir <a>Equipo 1</a> al Torneo: <a>Torneo 1</a></label>
-            </div>
-            <div class="checkbox checkbox-info">
-                <input id="checkbox4" type="checkbox">
-                    <label for="checkbox4">Rolando Romero desea inscribir <a>Equipo 2</a> al Torneo: <a>Torneo 2</a></label>
-            </div>
-            <div class="checkbox checkbox-info">
-                <input id="checkbox4" type="checkbox">
-                    <label for="checkbox4">Katia Santillan desea inscribir <a>Equipo 3</a> al Torneo: <a>Torneo 3</a></label>
-            </div>
-        </form>
-    <br>
-    </div>
-    <br>
-    <div style="options-div">
-        <center>
-            <button type="button" onclick="confirm('¿Seguro que desea ACEPTAR la solicitud?');"class="btn btn-success">Aceptar</button>
-            <button type="button" onclick="confirm('¿Seguro que desea RECHAZAR la solicitud?');"class="btn btn-danger btn-rounded">Rechazar</button>
-        </center>
-    </div> 
-</div>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <table class="table table-striped ">
+            <thead>
+            <tr>
+                <th>Elegir</th>
+                <th>Equipo</th>
+                <th>Capitán</th>
+                <th>Torneo</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            
+            ?>
+            </tbody>
+        </table>
+                     <div class="row center-xs center-sm center-md center-lg">
+                        <div class="ol-xs-3 col-md-3 col-lg-3">
+                            <div class="box">
+                                <button id="cancelarInvitacion" type="submit" class="btn btn-success" data-type="alerta">Aceptar</button>
+                            </div>
+                        </div>
+                        <div class="col-xs-3 col-md-3 col-lg-3">
+                            <div class="box">
+                                <button id="aceptarInvitacion" type="button" class="btn btn-danger" data-type="alerta2">Rechazar</button>
+                            </div>
+                        </div>
+                    </div>
+    </form>
+ </div>
+    <br/>
+    <hr>
+    <br/>
