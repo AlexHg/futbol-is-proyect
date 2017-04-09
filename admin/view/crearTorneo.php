353bd2e27decf0c413da3e75039a85bda59ad52b
AMPS95 <amarianaps95@gmail.com>
2017-04-09 13:20:28 -0500

    Update crearTorneo.php

--- admin/view/crearTorneo.php
+++ admin/view/crearTorneo.php
@@ -1,27 +1,84 @@
 
-<div id="content-title">
-    <h2>Datos del Torneo</h2>
-</div>
-<div id="content-body">
-    <center>
-        <form class="form-control" role="form" style="width: 50%">
-            <div class="form-group" >
-                <label class="control-label">Nombre del Torneo</label>
-                <div class="form-control-cont">
-                    <input type="text" class="form-control full" value="Nombre">
+ <section id="content">
+                <div id="content-title">
+                    <h2>Crear Torneo</h2>
                 </div>
-                <label class="control-label">Cantidad de grupos</label>
-                <div class="form-control-cont">
-                    <select multiple="" class="form-control full">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                    </select>
+                <div id="content-body">
+                <form class="form-control" role="form" style="width: 90%">
+                        <div class="form-group">
+                            <label class="control-label">Nombre de Torneo</label>
+                                <div class="form-control-cont">
+                                    <input type="text" class="form-control full" placeholder="Nombre">
+                                </div>
+                            </div>
+                       <h1>
+                       ***CAMBIAR COMBOS DE FECHAS A CALENDAR</h1>
+                        <div class="form-group">
+                            <label class="control-label">Fecha de Inicio</label>
+                            <div class="form-control-cont">
+                                <select class="form-control full">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                        </div>
+                         <h1>**CAMBIAR COMBOS DE FECHAS A CALENDAR</h1>
+                        <div class="form-group">
+                            <label class="control-label">Fecha límite de inscripción</label>
+                            <div class="form-control-cont">
+                                <select class="form-control full">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Tipo de Torneo</label>
+                            <div class="form-control-cont">
+                                <select class="form-control full">
+                                    <option>Soccer</option>
+                                    <option>Rápido</option>
+                                </select>
+                            </div>
+                        </div>
+                         <h1>Días de Juego</h1>
+                        <div class="checkbox checkbox-primary">
+                                    <input id="checkbox2" type="checkbox" checked="">
+                                    <label for="checkbox2">
+                                       Lunes
+                                    </label>
+                        </div>
+                          <div class="checkbox checkbox-primary">
+                                    <input id="checkbox2" type="checkbox" checked="">
+                                    <label for="checkbox2">
+                                       Martes
+                                    </label>
+                        </div>
+                          <div class="checkbox checkbox-primary">
+                                    <input id="checkbox2" type="checkbox" checked="">
+                                    <label for="checkbox2">
+                                       Miércoles
+                                    </label>
+                        </div>
+                          <div class="checkbox checkbox-primary">
+                                    <input id="checkbox2" type="checkbox" checked="">
+                                    <label for="checkbox2">
+                                       Jueves
+                                    </label>
+                        </div>
+                          <div class="checkbox checkbox-primary">
+                                    <input id="checkbox2" type="checkbox" checked="">
+                                    <label for="checkbox2">
+                                       Viernes
+                                    </label>
+                        </div>
+                        </form>
+                    <center><button type="button" class="btn btn-success">Crear</button></center>
                 </div>
-            </div>
-            <br>
-            <button type="button" class="btn btn-success" onclick="alert('Torneo Creado Exitosamente')">Crear Torneo</button>
-        </form>
-    </center>
-</div>
+          </section>
