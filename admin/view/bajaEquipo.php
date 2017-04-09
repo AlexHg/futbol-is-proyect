2409434be02a2180d3fdef923fc1a3b17b60aa73
AMPS95 <amarianaps95@gmail.com>
2017-04-09 14:32:32 -0500

    Update bajaEquipo.php

--- admin/view/bajaEquipo.php
+++ admin/view/bajaEquipo.php
@@ -91,4 +91,44 @@
     
     
 </script>
+  
+  <!--
 
+Consultas para esta pantalla
+
+Para mostrar el nombre de los equipos (para el combobox)
+select NombreEquipo from equipo e,equipo_torneo et where et.idequipo=e.idequipo;
+Para mostrar el nombre del capitan para el equipo que se seleccione
+select Nombre,Apellidos from Usuario where correo="elcorreodelcapitan";
+mostrar los torneos donde se ha registrado este equipo
+select T.nombre from torneo t, equipo_torneo et, equipo e where e.idequipo=et.idequipo and t.idtorneo=et.idtorneo and e.correo="el correo del capitan";
+y despues se van a hacer estas consultas para eliminar el equipo del torneo las primeras 2 son para conocer el id del torneo y el id del equipo
+select IDTorneo from torneo where Nombre="elnombredeltorneo";
+select IDEquipo from equipo where correo="elcorreodelcapitan";
+delete from equipo_torneo where IDEquipo=1 and IDTorneo=1;
+
+Mensajes
+
+    <script> <div class="overlay-container">
+                        <div class="window-container alerta">
+                            <h3>¿Esta seguro que desea eliminar el equipo?</h3> 
+                            Se eliminará toda la información generada para este equipo en el torneo<br/>
+                            <br/>
+                            <center>
+							<button class="btn btn-success close">Aceptar</button>
+                            <button class="btn btn-danger close" >Cancelar</button>
+                            </center>
+                        </div>
+          </div></script>
+	
+    <script> <div class="overlay-container">
+                        <div class="window-container alerta">
+                            <h3>Operación realizada exitosamente</h3> 
+                            La operación se realizó exitosamente<br/>
+                            <br/>
+                            <center>
+							<button class="btn btn-success close">Aceptar</button>
+                            </center>
+                        </div>
+          </div></script>
+-->
