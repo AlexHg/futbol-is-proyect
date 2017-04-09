7940cb93aa86e59c83a12b15d45a1d19461e9802
AMPS95 <amarianaps95@gmail.com>
2017-04-09 15:23:30 -0500

    Create consultarTablaPosiciones.php

---
+++ admin/view/consultarTablaPosiciones.php
@@ -0,0 +1,92 @@
+<div id="content-title">
+    <h2>Consultar Tabla de Posiciones</h2>
+</div>
+<div id="content-body">
+                    <div class="form-group">
+                            <label class="control-label">Nombre de Torneo</label>
+                            <div class="form-control-cont">
+                                <select class="form-control full">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                        </div>
+                        
+  <button onclick="window.location.href='#'" type="button" class="btn btn-info">Vista general del torneo</button>
+    <table class="table table-striped ">
+        <thead>
+            <tr>
+                <th>Equipo</th>
+                <th>Juegos Ganados</th>
+                <th>Juegos Perdidos</th>
+                <th>Juegos Empatados</th>
+                <th>Goles a favor</th>
+                <th>Goles en Contra</th>
+                <th>Goles en Contra</th>
+                <th>Diferencia</th>
+                <th>Puntos Acomulados</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">Inauguración</th>
+                <td>19/09/16</td>
+                <td>A (12:30)</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">Jornada 1</th>
+                <td>Del 19/09/16 al 23/09/16</td>
+                <td>A (13:30)</td>
+                <td>B (12:30 y 13:30)</td>
+                <td>C (12:30 y 13:30)</td>
+                <td>D (12:30 y 13:30)</td>
+            </tr>
+            <tr>
+                <th scope="row">Jornada 2</th>
+                <td>Del 26/09/16 al 30/09/16</td>
+                <td>A (12:30 y 13:30)</td>
+                <td>B (12:30 y 13:30)</td>
+                <td>C (12:30 y 13:30)</td>
+                <td>D (12:30 y 13:30)</td>
+            </tr>
+            <tr>
+                <th scope="row">Jornada 3</th>
+                <td>Del 03/10/16 al 07/10/16</td>
+                <td>A (12:30 y 13:30)</td>
+                <td>B (12:30 y 13:30)</td>
+                <td>C (12:30 y 13:30)</td>
+                <td>D (12:30 y 13:30)</td>
+            </tr>
+            <tr>
+                <th scope="row">Cuartos de final</th>
+                <td>Del 11/10/16 al 13/10/16</td>
+                <td></td>
+                <td><div>A1 vs B1 (12:30) (W)</div><div> B1 vs A2 (13:30) (X)</div></td>
+                <td></td>
+                <td><div>C1 vs D2 (12:30) (Y)</div><div> D1 vs C2 (13:30) (Z)</div></td>
+            </tr>
+            <tr>
+                <th scope="row">Semifinal</th>
+                <td>18/10/16</td>
+                <td></td>
+                <td><div>(W) vs (Y) (12:30)</div><div>(X) vs (Z) (13:30)</div></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">Final</th>
+                <td>26/10/16</td>
+                <td></td>
+                <td></td>
+                <td><div>Ganador (W vs Y)</div><div>vs</div><div>Ganador (W vs Z)</div></td>
+                <td></td>
+            </tr>                                                                                          
+        </tbody>
+    </table>  
+</div>
