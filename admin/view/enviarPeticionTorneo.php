e308e12f6927a39e154f6563fb489388e3427c8f
mayradatos <mayra.patricia@hotmail.com>
2017-04-09 21:03:36 -0500

    Update enviarPeticionTorneo.php

--- admin/view/enviarPeticionTorneo.php
+++ admin/view/enviarPeticionTorneo.php
@@ -36,3 +36,27 @@
     </center>
 </form>
 </div>
+<!--
+<script> <div class="overlay-container">
+                        <div class="window-container alerta">
+                            <h3>Cancelar Operación</h3> 
+                            Estas seguro de deshacer la operación en curso<br/>
+                            <br/>
+                            <center>
+							<button class="btn btn-success close">Aceptar</button>
+                            <button class="btn btn-danger close" >Cancelar</button>
+                            </center>
+                        </div>
+          </div></script>
+    <script> <div class="overlay-container">
+                        <div class="window-container alerta">
+                            <h3>Confirmación de envio</h3> 
+                            ¿Estas seguro de realizar esta operación, cambiara tu perfil de usuario?<br/>
+                            <br/>
+                            <center>
+							<button class="btn btn-success close">Aceptar</button>
+                            <button class="btn btn-danger close" >Cancelar</button>
+                            </center>
+                        </div>
+          </div></script>
+-->
