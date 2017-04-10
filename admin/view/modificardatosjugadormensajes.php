f42ea2761bc445c5723070ea7861088b82c96dc0
mayradatos <mayra.patricia@hotmail.com>
2017-04-09 21:16:22 -0500

    Create modificardatosjugadormensajes.php

---
+++ admin/view/modificardatosjugadormensajes.php
@@ -0,0 +1,42 @@
+    <script> <div class="overlay-container">
+                        <div class="window-container alerta">
+                            <h3>Confirmación de Modificación</h3> 
+                            ¿Estás seguro de realizar esta modificación?<br/>
+                            <br/>
+                            <center>
+							<button class="btn btn-success close">Aceptar</button>
+                            <button class="btn btn-danger close" >Cancelar</button>
+                            </center>
+                        </div>
+          </div></script>
+    <script> <div class="overlay-container">
+                        <div class="window-container alerta">
+                            <h3>Operación realizada exitosamente</h3> 
+                            La operación se realizó exitosamente<br/>
+                            <br/>
+                            <center>
+							<button class="btn btn-success close">Aceptar</button>
+                            </center>
+                        </div>
+          </div></script>
+    <script> <div class="overlay-container">
+                        <div class="window-container alerta">
+                            <h3>Confirmación de cancelación</h3> 
+                            ¿Estás seguro de cancelar esta modificación?<br/>
+                            <br/>
+                            <center>
+							<button class="btn btn-success close">Aceptar</button>
+                            <button class="btn btn-danger close" >Cancelar</button>
+                            </center>
+                        </div>
+          </div></script>
+    <script> <div class="overlay-container">
+                        <div class="window-container alerta">
+                            <h3>Información no valida</h3> 
+                            La información introducida no puede almacenarse. revisela<br/>
+                            <br/>
+                            <center>
+							<button class="btn btn-danger close">Reintentar</button>
+                            </center>
+                        </div>
+          </div></script>
