fafd4fb27af42214b05fa7f5efb012d750660bcb
AMPS95 <amarianaps95@gmail.com>
2017-04-12 17:52:21 -0500

    Update creditos.php

--- admin/view/creditos.php
+++ admin/view/creditos.php
@@ -5,7 +5,18 @@
 <div id="content-body" style="display:flex; justify-content:center; align-items:center; min-height:60vh;">
     
     <ul>
-        <li>Alejandro Hernandez Gomez</li>
+        <li>Hernandez Gomez Alejandro </li>
+        <li>Santillán Álvarez Katia </li>
+        <li>Tovar Barba Mayra Patricia </li>
+        <li>Pérez Soberanes Alina Mariana </li>
+        <li>González González Oscar Arturo </li>
+        <li>Romero Téllez Rolando  </li>
+        <li>Durán Juárez Jesús Alberto </li>
+        <li>Hernandez Gutiérrez Alan  </li>
+        <li>Santuario Parra Luis Fernando </li>
+        <li>Castro Maya Christopher </li>
+        <li>Hernández Bautista Yasmine Pilar </li>
+        <li>Diego Cruz Mondragón </li>
     </ul>
     <br><br>
 </div>
