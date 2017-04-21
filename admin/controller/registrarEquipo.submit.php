a169495a0847cb970c8669be1bcce564802a59b3
Luis Fernando Santuario Parra <santuarioparral@hotmail.com>
2017-04-20 21:22:05 -0500

    arreglado registrar equipo

--- admin/controller/registrarEquipo.submit.php
+++ admin/controller/registrarEquipo.submit.php
@@ -7,8 +7,8 @@ $Correo = $_POST["email"];
 $Desc = $_POST["uniforme"];
 
 if(Jugador::registrarEquipo($Correo, $Equipo, $Desc) ){
-	start(array("role"=>2, "rolename"=>"Capitan", "name" => $_SESSION["name"], "email" => $_SESSION["email"], "avatar" => $_SESSION["avatar"]));
-	Flight::redirect('/registrarEquipo?n=exito');
+	Session::start(array("role"=>2, "rolename"=>"Capitan", "name" => $_SESSION["name"], "email" => $_SESSION["email"], "avatar" => $_SESSION["avatar"]));
+	Flight::redirect('/home?n=exito');
 }else{
 	Flight::redirect('/registrarEquipo?n=error');
 }
