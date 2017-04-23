cbeae2ac5f9909b67b3a6abc8e5f0d87d5d63682
AMPS95 <amarianaps95@gmail.com>
2017-04-23 14:30:26 -0500

    Update registrarResultados.php

--- admin/view/registrarResultados.php
+++ admin/view/registrarResultados.php
@@ -82,29 +82,67 @@
                         <br>
                         
                           <br>
-                       
-							<div class="form-group" >
-                            <label class="control-label">Equipo 1</label>
-                                <div class="form-control-cont">
-                                    <input type="text" class="form-control full" value="Goles Anotados">
-                                </div>
+			    <div class="form-group">
+                            <label class="control-label">Equipo 1 </label>
+                            <div class="form-control-cont">
+                                <select class="form-control full">
+                                    <option>1</option>
+                                    <option>2</option>
+				    <option>3</option>
+                                    <option>4</option>
+				    <option>5</option>
+                                    <option>6</option>
+				    <option>7</option>
+                                    <option>8</option>
+				    <option>9</option>
+                                    <option>10</option>
+				    <option>11</option>
+                                    <option>12</option>
+				    <option>13</option>
+                                    <option>14</option>
+				    <option>15</option>
+                                    <option>16</option>
+				    <option>17</option>
+                                    <option>18</option>
+				    <option>19</option>
+                                    <option>20</option>
+                                </select>
                             </div>
-                        
-                            <br>
-                           
-                            <div class="form-group" >
-                            <label class="control-label">Equipo 2</label>
-                                <div class="form-control-cont">
-                                    <input type="text" class="form-control full" value="Goles Anotados">
-                                </div>
+                   	</div>
+                        <br>
+			 <div class="form-group">
+                            <label class="control-label">Equipo 1 </label>
+                            <div class="form-control-cont">
+                                <select class="form-control full">
+                                    <option>1</option>
+                                    <option>2</option>
+				    <option>3</option>
+                                    <option>4</option>
+				    <option>5</option>
+                                    <option>6</option>
+				    <option>7</option>
+                                    <option>8</option>
+				    <option>9</option>
+                                    <option>10</option>
+				    <option>11</option>
+                                    <option>12</option>
+				    <option>13</option>
+                                    <option>14</option>
+				    <option>15</option>
+                                    <option>16</option>
+				    <option>17</option>
+                                    <option>18</option>
+				    <option>19</option>
+                                    <option>20</option>
+                                </select>
                             </div>
-                            </div>
+                   	</div>
                               
                             <div align="center">
                             	
 					<button type="button" class="btn btn-success" onclick="myFunction3()">Guardar Cambios</button>
 							</div>
-							</form> 
+		</form> 
                         <br>
                         <br>
                     </div>
