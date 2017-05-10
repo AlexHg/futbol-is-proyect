f2a7c9e7de7e9f29befa0400bf42a501f5c8d1cb
Alejandro Hernandez <alexxh42@gmail.com>
2017-05-10 16:14:50 -0500

    reportes

--- admin/view/resumen.php
+++ admin/view/resumen.php
@@ -48,17 +48,12 @@
 <script>
     var Tablas = "<h1>Reporte del club de futbol</h1><br><br>"+document.querySelector("#content-body").outerHTML;
     function descargarExcel(variable_conTabla){
-        //Creamos un Elemento Temporal en forma de enlace
         var tmpElemento = document.createElement('a');
-        // obtenemos la información desde el div que lo contiene en el html
-        // Obtenemos la información de la tabla
         var data_type = 'data:application/vnd.ms-excel;';
         var tabla_div = variable_conTabla;
         var tabla_html = tabla_div.replace(/ /g, '%20');
         tmpElemento.href = data_type + ', ' + tabla_html;
-        //Asignamos el nombre a nuestro EXCEL
         tmpElemento.download = 'Donativos.xls';
-        // Simulamos el click al elemento creado para descargarlo
         tmpElemento.click();
     }
 </script>
