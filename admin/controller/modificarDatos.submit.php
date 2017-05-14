16d9ceb52d091b2da259df21b6a965283f5c6eef
Alejandro Hernandez <alexxh42@gmail.com>
2017-05-14 16:21:24 -0500

    Moficar datos jugador
    ya se cambia la sesión cuando modificas datos

--- admin/controller/modificarDatos.submit.php
+++ admin/controller/modificarDatos.submit.php
@@ -8,6 +8,15 @@
 	$correo = $_POST["mail"];
 	
 	if(Usuario::updateDatosUsuario($nombre, $apellido, $telefono, $contrasena, $correo)){
+        Session::start(
+            array(
+                "role"=>$_SESSION["role"], 
+                "rolename"=>$_SESSION["rolename"], 
+                "name" => $_POST["name"]." ".$_POST["lastname"], 
+                "email" => $_SESSION["email"], 
+                "avatar" => $_SESSION["avatar"]
+            )
+        );
         Flight::redirect('/modificarDatos?n=done');
     }else{
         Flight::redirect('/modificarDatos?n=err');
