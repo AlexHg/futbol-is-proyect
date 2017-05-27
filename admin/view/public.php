b5652f0a613eb9dc509a2100b2781aeb256115e6
AMPS95 <amarianaps95@gmail.com>
2017-05-27 18:51:32 -0500

    Update public.php

--- admin/view/public.php
+++ admin/view/public.php
@@ -66,7 +66,6 @@
 
                                 <li class="active"><a href="#">Inicio</a></li>
                                 <li><a href="#">Conócenos</a></li>
-                                <li><a href="#"> Reglamentos Generales </a></li>
                                 <li><a href="#">Contáctanos</a></li>
                                 <li><a href="home">Acceder a mi cuenta</a></li>
                                 <li><a href="registrarCuenta">Registrarse</a></li>
